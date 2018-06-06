f42b631f7c1c0346baf39dc36855471c4e4f5916
Tobias Weissert <me@supertoub.ch>
2018-06-06 21:19:32 +0200

    initial commit

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install thefuck
+fuck
+fuck
+brew install git
+brew install node
+
+brew cask install google-chrome
+brew cask install dropbox
+brew cask install transmit
+brew cask install flux
+brew cask install sketch
+brew cask install paw
+brew cask install whatsapp
+brew cask install telegram
+brew cask install alfred
+brew cask install veertu
+brew cask install 1password
+brew cask install visual-studio-code
+brew cask install adobe-creative-cloud
+brew cask install slack
+brew cask install airserver
